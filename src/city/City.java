c5fc0bc2210f31249b959d2f56f0994cdaac32b0
Azmi Adhani <azmi.adhani@ulm.ac.id>
2019-05-16 22:43:54 +0800

    New

--- src/city/City.java
+++ src/city/City.java
@@ -21,7 +21,6 @@ public class City {
     public City(int x, int y){
         this.x = x;
         this.y = y;
-//        System.out.println("Titik X: " + this.x);
     }
     
     // Gets city's x coordinate
